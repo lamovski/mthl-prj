4364491a37bd37539d389f21a5861e740f7fc275
v. lamovski <49014093+lamovski@users.noreply.github.com>
2020-10-20 18:45:52 +0700

    ruined

--- dividing.py
+++ dividing.py
@@ -1,17 +1,14 @@
-def algev(x, y):
-    if(x % y == 0):
-        return y;
-    algev(y, x % y)
-
-def nod(x, y):
-    if(x>y):
+def # if(x % y == 0):
+  3
+def# nod(x, y):
+  #  if(x>y):
         return algev(x, y)
-    return algev(y, x)
+ #   return algev(y, x)
+#
+de#f nok(x, y):
+ #   return x*y//nod(x, y)
 
-def nok(x, y):
-    return x*y//nod(x, y)
-
-x = int(input())
-y = int(input())
-print(nod(x, y))
-print(nok(x, y))
+#x = int(input())
+#y = int(input())
+#print(nod(x, y))
+#print(nok(x, y))
