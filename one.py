c399c0764ab29d9149898b790663a33d47182e48
v. lamovski <49014093+lamovski@users.noreply.github.com>
2020-11-10 18:55:56 +0700

    Update one.py

--- one.py
+++ one.py
@@ -1,9 +1,21 @@
-l = []
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    l.append(n)
-s=set(l)
+class Bruh:
+    bruh_amount = 0
+    bruh_list = []
 
-print(len(s))
+    def __init__(self, name):
+        self.name = name
+
+    def add_bruh(self, bruh):
+        self.bruh_list.append(bruh)
+        bruh.bruh_list.append(self)
+
+    def say(self):
+        print('bruh')
+
+joe = Bruh('joe')
+jack = Bruh('jack')
+jack.add_bruh(joe)
+joe.say()
+
+print(jack.bruh_list)
+print(joe.bruh_list)
