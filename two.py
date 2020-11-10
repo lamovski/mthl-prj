aaf555e10076b9bbd77089b3eaef5180b3086672
v. lamovski <49014093+lamovski@users.noreply.github.com>
2020-11-10 18:58:36 +0700

    Update two.py

--- two.py
+++ two.py
@@ -1,9 +1,16 @@
-s = input()
-l = s.split(' ')
-set = set()
-for i in l:
-    if set.__contains__(i):
-        print("NO")
-    else:
-        print("YES")
-    set.add(i)
+class One:
+    def __init__(self, name):
+        self.name = name
+
+class Two:
+    def __init__(self, name):
+        self.name = name
+
+    def perform(self, one):
+        one.name = '...'
+        print(self.name, 'performed', one.name)
+
+one = One('text')
+two = Two('stuff')
+print(one.name)
+two.perform(one)
