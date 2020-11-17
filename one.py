c810d10e966ab0794adb28375e037a42411601df
v. lamovski <49014093+lamovski@users.noreply.github.com>
2020-11-17 19:23:00 +0700

    Update one.py

--- one.py
+++ one.py
@@ -1,21 +1,25 @@
-class Bruh:
-    bruh_amount = 0
-    bruh_list = []
+# -*- coding: utf8 -*-
+import re
+import nltk
+from nltk.corpus import stopwords
+from string import punctuation
 
-    def __init__(self, name):
-        self.name = name
+txt = 'Не угощай и не потчевай никого, а веди себя лучше так, чтобы тебя угощали, а больше всего береги и копи копейку: эта вещь надежнее всего на свете. Товарищ или приятель тебя надует и в беде первый тебя выдаст, а копейка не выдаст, в какой бы беде ты ни был. Все сделаешь и все прошибешь на свете копейкой.'
 
-    def add_bruh(self, bruh):
-        self.bruh_list.append(bruh)
-        bruh.bruh_list.append(self)
+sentences = nltk.sent_tokenize(txt, 'russian')
+print('Самое длинное предложение:', max(sentences))
+print('Самое короткое предложение:', min(sentences))
 
-    def say(self):
-        print('bruh')
+nptxt = txt.lower()
+nptxt = re.sub("[\,\.]", "", nptxt)
 
-joe = Bruh('joe')
-jack = Bruh('jack')
-jack.add_bruh(joe)
-joe.say()
+words = nltk.word_tokenize(nptxt, 'russian')
+print('Самые частые слова со стоп-словами:', nltk.FreqDist(words).most_common(3))
 
-print(jack.bruh_list)
-print(joe.bruh_list)
+nswords = [word for word in words if word not in stopwords.words('russian')]
+
+print('И без:', nltk.FreqDist(nswords).most_common(3))
+
+print(nltk.pos_tag(nswords, lang = 'rus'))
+
+print(list(nltk.FreqDist(nltk.bigrams(nswords)).most_common(3)))
